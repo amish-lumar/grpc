87b1c17b989838665bb2f3535d388b9f7d1a0394
Amish Kumar <amish.kumar@walmartlabs.com>
2020-05-02 23:10:25 +0530

    post big object

--- src/main/java/com/poc/grpc/controller/HelloController.java
+++ src/main/java/com/poc/grpc/controller/HelloController.java
@@ -1,7 +1,12 @@
 package com.poc.grpc.controller;
 
+import com.poc.grpc.dto.DailyShippedOrders_v2;
 import com.poc.grpc.dto.HelloResponse;
+import com.poc.grpc.service.HelloServiceImpl;
+import com.poc.grpc.service.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,10 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private OrderServiceImpl orderService;
+
+    @Autowired
+    private HelloServiceImpl helloService;
+
     @GetMapping("/hellowithrest")
     public HelloResponse helloWithRest(){
-        HelloResponse response = new HelloResponse();
-        response.setGreeting("hello from rest");
-        return  response;
+        return helloService.hello();
+    }
+
+    @PostMapping("/orderwithrest")
+    public HelloResponse processOrderWithRest(DailyShippedOrders_v2 request){
+        return orderService.processOrderWithRest(request);
     }
 }
--- src/main/java/com/poc/grpc/service/OrderServiceImpl.java
+++ src/main/java/com/poc/grpc/service/OrderServiceImpl.java
@@ -1,7 +1,8 @@
 package com.poc.grpc.service;
 
+import com.poc.grpc.generate.DailyShippedOrders_v2;
 import com.poc.grpc.generate.HelloResponse;
-import com.poc.grpc.generate.HelloServiceGrpc;
+import com.poc.grpc.generate.OrderServiceGrpc;
 import org.lognet.springboot.grpc.GRpcService;
 
 import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
@@ -10,16 +11,13 @@ import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
  * @author a0k02hz
  */
 @GRpcService
-public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
+public class OrderServiceImpl extends OrderServiceGrpc.OrderServiceImplBase {
 
     @Override
-    public void hello(com.poc.grpc.generate.HelloRequest request,
-                      io.grpc.stub.StreamObserver<com.poc.grpc.generate.HelloResponse> responseObserver) {
+    public void processOrder(com.poc.grpc.generate.DailyShippedOrders_v2 request,
+                             io.grpc.stub.StreamObserver<com.poc.grpc.generate.HelloResponse> responseObserver) {
         String greeting = new StringBuilder()
-                .append("Hello, ")
-                .append(request.getFirstName())
-                .append(" ")
-                .append(request.getLastName())
+                .append("Order processing, ")
                 .toString();
 
         HelloResponse response = HelloResponse.newBuilder()
@@ -30,9 +28,10 @@ public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
         responseObserver.onCompleted();
     }
 
-    public com.poc.grpc.dto.HelloResponse hello(){
+    public com.poc.grpc.dto.HelloResponse processOrderWithRest(com.poc.grpc.dto.DailyShippedOrders_v2 request){
         com.poc.grpc.dto.HelloResponse response = new com.poc.grpc.dto.HelloResponse();
-        response.setGreeting("hello from rest");
+        response.setGreeting("Order processing from rest");
         return  response;
     }
+
 }
